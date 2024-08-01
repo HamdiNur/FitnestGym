e488f24275e79cf90144d7c88c15384656658b72
HamdiNur <hamdoonur26@gmail.com>
2024-08-01 12:09:12 +0300

    static hero section  finished

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import './Hero.css'
 import Header from '../Header/Header';
+import hero_image from "../../assets/hero_image.png"
+import hero_Image_back from "../../assets/hero_image_back.png"
+import Heart from "../../assets/heart.png"
+import Calories from "../../assets/calories.png"
 const Hero = () => {
   return (
     <div className='hero'>
@@ -57,9 +61,29 @@ const Hero = () => {
           <buttons className="btn">Learn More</buttons>
         </div>
       </div>
-      <div className="right-hero">rightside</div>
+      <div className="right-h">
+        <button className='btn'> Join Now</button>
+   
+         <div className="heart-rate">
+          <img src={Heart} alt="" />
+          <span>Heart Rate</span><span>116 bpm</span>
+         </div>
 
 
+           {/* Hero Images */}
+         <img src={hero_image} alt=""  className='hero-image'/>
+        <img src={hero_Image_back} alt="" className='hero_image-back' />
+
+        {/* Calories */}
+        <div className="calories">
+          <img src={Calories} alt="" />
+          <div>
+          <span>CaloriesBurned</span>
+          
+          <span>220 kcal</span>
+        </div>
+        </div>
+      </div>
     </div>
   );
 }
