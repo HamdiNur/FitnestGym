a60af2a01d277c2fcef4b92da7618186c7867812
HamdiNur <hamdoonur26@gmail.com>
2024-08-02 20:28:36 +0300

    Imported new components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 import Hero from './components/Hero/Hero';
+import Plans from './components/Plans/Plans';
+import Program from './components/Program/Program';
+import Reasons from './components/Reasons/Reasons';
+import Testmonial from './components/Testmonial/Testmonial';
+
 
 
 function App() {
   return (
     <div className="App">
       <Hero/>
+      <Program/>
+      <Reasons/>
+      <Plans/>
+      <Testmonial/>
       
     </div>
   );
